1abaef1ae14590351c79e9dfb3f7e1082a9d5ab2
Santhosh Kumar S <santhosh2701kumar@gmail.com>
2025-07-21 11:52:09 +0530

    query support2

--- web_fetcher.py
+++ web_fetcher.py
@@ -21,7 +21,9 @@ def get_top_news(api_key, query="technology", language="en"):
 def get_stock_blog_rss(rss_url="https://finance.yahoo.com/news/rssindex"):
     try:
         feed = feedparser.parse(rss_url)
-        return [{"title": entry.title, "link": entry.link} for entry in feed.entries]
+        # return [{"title": entry.title, "link": entry.link} for entry in feed.entries]
+        top_headlines = newsapi.get_top_headlines(q=query, language=language)
+        return [article['title'] for article in top_headlines['articles']]
     except Exception as e:
         return [{"title": f"Error fetching RSS: {e}", "link": "#"}]
 
